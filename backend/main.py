2706a056289c0531f2cb3da412f8385a1dc7d1b3
Roni17-cyber <ronishkarki17@gmail.com>
2026-05-22 00:06:41 -0500

    feat: add post and get expenses endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import date 
 
 app = FastAPI()
+class Expense(BaseModel):
+    amount: float
+    note: str
+    category: str
+    date: date
+
+expenses = []
 
 @app.get("/")
 def root():
     return {"message": "SpendSense API is running!"}
+
+@app.post("/expenses")
+def add_expense(expense: Expense):
+    expenses.append(expense)
+    return {"message": "Expense added!", "expense": expense}
+@app.get("/expenses")
+def get_expenses():
+    return expenses
